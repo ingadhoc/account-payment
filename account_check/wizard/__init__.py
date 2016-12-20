1f5ffcae11a14b9378b59ca8c3b1311891074e4e
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-12-20 10:25:46 -0300

    DEL old wizard of checks

--- account_check/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in module root
-# directory
-##############################################################################
-# from . import check_action
-# from . import check_reject
-# from . import change_check
