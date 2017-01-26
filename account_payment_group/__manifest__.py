2fb73359c44494d988380ea291dbb1482aef5357
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-01-26 09:48:46 -0600

    VITTAR-24 [FIX] Company Settings

--- account_payment_group/__manifest__.py
+++ account_payment_group/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment with Multiple methods",
-    "version": "9.0.1.1.0",
+    "version": "10.0.1.1.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA, Odoo Community Association (OCA)",
