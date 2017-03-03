335559f5d83b863bc0cf43c2a65635a882027a1e
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-03 17:15:57 -0600

    VITTAR-46 [IMP] Constraint

--- account_check/models/account_journal.py
+++ account_check/models/account_journal.py
@@ -19,11 +19,19 @@ class AccountJournal(models.Model):
     @api.onchange('outbound_payment_method_ids', 'inbound_payment_method_ids')
     def change_payment_method(self):
         payment_method = self.outbound_payment_method_ids.ids + self.inbound_payment_method_ids.ids
-        if (4 in payment_method and 5 in payment_method) or (6 in payment_method and 8 in payment_method):
-            self.update({
-                'outbound_payment_method_ids':[],
-                'inbound_payment_method_ids': [],
-            })
+        if (4 in payment_method and 5 in payment_method) or if (6 in payment_method and 8 in payment_method):
+            if (4 in payment_method and 5 in payment_method):
+                for payment_method_type in self.outbound_payment_method_ids, self.inbound_payment_method_ids:
+                    for line in payment_method_type:
+                        if line.id == 4 or line.id == 5:
+                        line.unlink()
+            
+            if (6 in payment_method and 8 in payment_method):
+                for payment_method_type in self.outbound_payment_method_ids, self.inbound_payment_method_ids:
+                    for line in payment_method_type:
+                        if line.id == 6 or line.id == 8:
+                        line.unlink()
+                
             raise UserError(_('A journal cannot have any of these two types at the same time, Own Check and 3rd Party Check, or Check (Own or 3rd Party) and Withholding. Please correct your selection in "Advanced Settings" tab.'))
     
     
