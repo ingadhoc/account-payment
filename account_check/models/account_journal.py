b739044323687c7d1491174dc75a48ae01cd2de2
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-03 17:56:39 -0600

    VITTAR-46 [FIX] Constraint

--- account_check/models/account_journal.py
+++ account_check/models/account_journal.py
@@ -21,13 +21,11 @@ class AccountJournal(models.Model):
         payment_method = self.outbound_payment_method_ids.ids + self.inbound_payment_method_ids.ids
         if (4 in payment_method and 5 in payment_method) or (6 in payment_method and 8 in payment_method):
             if (4 in payment_method and 5 in payment_method):
-                for payment_method_type in [ 'outbound_payment_method_ids', 'inbound_payment_method_ids' ]:
-                    self.write({payment_method_type: [(3, 4)]})
-                    self.write({payment_method_type:[(3, 5)]})            
+                self.write({'inbound_payment_method_ids': [(3, 4)],
+                            'outbound_payment_method_ids':[(3, 5)]})            
             if (6 in payment_method and 8 in payment_method):
-                for payment_method_type in [ 'outbound_payment_method_ids', 'inbound_payment_method_ids' ]:
-                    self.write({payment_method_type: [(3, 6)]})
-                    self.write({payment_method_type:[(3, 8)]})   
+                self.write({'inbound_payment_method_ids': [(3, 6)],
+                            'outbound_payment_method_ids':[(3, 8)]})   
                 
             raise UserError(_('A journal cannot have any of these two types at the same time, Own Check and 3rd Party Check, or Check (Own or 3rd Party) and Withholding. Please correct your selection in "Advanced Settings" tab.'))
     
