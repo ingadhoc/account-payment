9da72b7e385eb64a331f40a5cae5ddf15735711e
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-06 14:57:34 -0600

    VITTAR-46 [IMP] Check Validation

--- account_check/models/account_journal.py
+++ account_check/models/account_journal.py
@@ -18,14 +18,16 @@ class AccountJournal(models.Model):
     check_control = fields.Boolean(
         'Check Control', required=False, default=False,
     )
-
-    @api.onchange('outbound_payment_method_ids', 'inbound_payment_method_ids')
-    def change_payment_method(self):
-            payment_method = self.outbound_payment_method_ids.ids + self.inbound_payment_method_ids.ids
-            if (4 in payment_method and 5 in payment_method) or (6 in payment_method and 8 in payment_method):
-                self.check_control = True
-            else:
-                self.check_control = False            
+                
+    @api.one
+    @api.constrains('outbound_payment_method_ids', 'inbound_payment_method_ids')
+    def _check_payments_methods(self):
+        payment_method = self.outbound_payment_method_ids.ids + self.inbound_payment_method_ids.ids
+        if (4 in payment_method and 5 in payment_method) or (6 in payment_method and 8 in payment_method):
+            UserError(_('A journal cannot have any of these two types at the same time, Own Check and 3rd Party Check, or Check (Own or 3rd Party) and Withholding. Please correct your selection in "Advanced Settings" tab.'))
+        else:
+            pass
+                
     
     @api.model
     def create(self, vals):
@@ -85,8 +87,4 @@ class AccountJournal(models.Model):
                 'outbound_payment_method_ids': [
                     (4, delivered_third_check.id, None)],
             })
-            
-        _sql_constraints = [
-        ('check_payment_method', 'check(check_control!=False)', 'A journal cannot have any of these two types at the same time, Own Check and 3rd Party Check, or Check (Own or 3rd Party) and Withholding. Please correct your selection in "Advanced Settings" tab.')
-       ]
 
