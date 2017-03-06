b76438774d287d61d754c53a3eb4ba62c93e62cd
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-06 13:21:22 -0600

    VITTAR-46 [FIX] Check Validation

--- account_check/models/account_journal.py
+++ account_check/models/account_journal.py
@@ -30,8 +30,8 @@ class AccountJournal(models.Model):
             #    self.write({'inbound_payment_method_ids': [(5)],
             #                'outbound_payment_method_ids':[(5)]})
             #raise UserError(_('A journal cannot have any of these two types at the same time, Own Check and 3rd Party Check, or Check (Own or 3rd Party) and Withholding. Please correct your selection in "Advanced Settings" tab.'))
-            self.check_control = True
-        self.check_control = False
+                self.check_control = True
+            self.check_control = False
     
     @api.model
     def create(self, vals):
