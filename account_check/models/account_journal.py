22bc92f5642e3a24542d584bc3038700efe03777
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-06 13:10:05 -0600

    VITTAR-46 [ADD] Check Validation

--- account_check/models/account_journal.py
+++ account_check/models/account_journal.py
@@ -24,10 +24,7 @@ class AccountJournal(models.Model):
     def change_payment_method(self):
         #self.ensure_one()
         for rec in self:
-            try:
-                payment_method = rec.outbound_payment_method_ids.ids + rec.inbound_payment_method_ids.ids
-            except:
-                payment_method = []
+            payment_method = rec.outbound_payment_method_ids.ids + rec.inbound_payment_method_ids.ids
             if (4 in payment_method and 5 in payment_method) or (6 in payment_method and 8 in payment_method):
             #if (4 in payment_method and 5 in payment_method):
             #    self.write({'inbound_payment_method_ids': [(5)],
