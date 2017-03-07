93c3e76ad727f332423988ae673705c584ef4338
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-07 13:52:53 -0600

    VITTAR-46 [IMP] Constraints Validation

--- account_check/models/account_journal.py
+++ account_check/models/account_journal.py
@@ -24,13 +24,14 @@ class AccountJournal(models.Model):
     @api.constrains('outbound_payment_method_ids', 'inbound_payment_method_ids')
     def _check_payments_methods(self):
         payment_method = self.outbound_payment_method_ids.ids + self.inbound_payment_method_ids.ids
-        valid=False
         if ((8 in payment_method) and (len(payment_method) == 1)):
             return 0
-        if ((6 in payment_method and 7 in payment_method) and len(payment_method) == 2) or ((( 6 in payment_method) and len(payment_method) == 1) or (( 7 in payment_method) and len(payment_method) == 1)):
-            return 0
-        if ( 6 not in payment_method) or ( 7 not in payment_method) or ( 8 not in payment_method):
-            return 0
+        else:
+            if (((6 in payment_method and 7 in payment_method) and len(payment_method) == 2) or ((( 6 in payment_method) and len(payment_method) == 1) or (( 7 in payment_method) and len(payment_method) == 1))):
+                return 0
+            else:
+                if ( 8 not in payment_method) and (( 7 not in payment_method) or ( 8 not in payment_method)):
+                    return 0
         raise ValidationError(_('A journal cannot have any of these two types at the same time, Own Check and 3rd Party Check, or Check (Own or 3rd Party) and Withholding. Please correct your selection in "Advanced Settings" tab.'))
                 
     
