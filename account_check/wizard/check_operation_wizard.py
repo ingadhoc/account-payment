36c4b39593fe9ac2d9f2172a66d16d15bf3556a6
Salvatore Josué Trimarchi Pinto <salvatore.trimarchi@gmail.com>
2017-03-09 10:48:47 -0600

    VITTAR-77 [ADD] Supplier Debit Note

--- account_check/wizard/check_operation_wizard.py
+++ account_check/wizard/check_operation_wizard.py
@@ -154,13 +154,13 @@ class account_check_wizard(models.TransientModel):
             if exp_type == '3':
                 if amount <= 0:
                     raise UserError(_('You can\'t claim with Zero Amount!'))
-                else:
-                    return check.action_create_debit_note(
-                    'reclaimed', partner_type, check.partner_id, account, amount, account_company)
-            else:
-                #return check._add_operation('reclaimed', check)
+                #else:
                 return check.action_create_debit_note(
                 'reclaimed', partner_type, check.partner_id, account, amount, account_company)
+            #else:
+            #    return check._add_operation('reclaimed', check)
+                #return check.action_create_debit_note(
+                #'reclaimed', partner_type, check.partner_id, account, amount, account_company)
 
 
             
