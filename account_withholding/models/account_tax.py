15b7ae96dad0828a1c28a5c4473d37a3f65662b7
Allan Aguilar <aguilar@bacgroup.net>
2017-03-21 16:14:28 -0600

    [IMP] VITTAR-116 account_withholding, account_withholding_automatic, Accounting>>Settings>> Withholding Taxes >> New fields needed

--- account_withholding/models/account_tax.py
+++ account_withholding/models/account_tax.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class AccountTaxTemplate(models.Model):
@@ -28,3 +29,23 @@ class AccountTax(models.Model):
     amount = fields.Float(
         required=False
     )
+    journal_id = fields.Many2one('account.journal', 'Payment Journal')
+    sequence_id = fields.Many2one('ir.sequence', 'Sequence') #, required=True
+    min_retention_amount = fields.Float(digits=(16, 2), string='Minimum Withholding Amount')
+
+    @api.one
+    @api.constrains('journal_id')
+    def _check_journal_id(self):
+        found = False
+        for payment_method in self.journal_id.inbound_payment_method_ids:
+            if (payment_method.code == 'withholding'):
+                found = True
+                break
+        
+        for payment_method in self.journal_id.outbound_payment_method_ids:
+            if (payment_method.code == 'withholding'):
+                found = True
+                break
+
+        if found == False:
+            raise ValidationError(_('Withholding Taxes can only use "Payment Journal" with type "Withholding", please check the data entered.'))
