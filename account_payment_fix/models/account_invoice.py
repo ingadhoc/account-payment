e74bfeebebce76c90b3c3512d510b2709217b181
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-06-15 08:49:55 -0300

    IMP allow to make pamyents with accounts that are not reconcileable

---
+++ account_payment_fix/models/account_invoice.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.multi
+    def register_payment(
+            self, payment_line,
+            writeoff_acc_id=False, writeoff_journal_id=False):
+        """
+        Con esto arreglamos que los pagos puedan pagar contra una cuenta
+        no conciliable, arreglamos porque odoo manda a conciliar por mas
+        que no haya facturas y da error, entonces si no hay facturas
+        que no intente conciliar nada (lo usamos en sipreco esto por ej)
+        """
+        if not self:
+            return True
+        return super(AccountInvoice, self).register_payment(
+            payment_line, writeoff_acc_id=writeoff_acc_id,
+            writeoff_journal_id=writeoff_journal_id)
