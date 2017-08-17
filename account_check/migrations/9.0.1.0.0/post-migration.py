8b16579c2b5750528b093ee9eb08098862362806
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-08-17 18:29:09 -0300

    REF

--- account_check/migrations/9.0.1.0.0/post-migration.py
+++ account_check/migrations/9.0.1.0.0/post-migration.py
@@ -215,13 +215,14 @@ def change_issue_journals(env):
                     WHERE aml.id in %s)
                 """ % (new_journal_id, tuple(move_line_ids)),
             )
-        openupgrade.logged_query(cr, """
-            UPDATE
-                account_payment
-            SET
-                journal_id=%s
-            WHERE id in %s
-            """ % (new_journal_id, tuple(wrong_checks.ids)),)
+        if wrong_checks:
+            openupgrade.logged_query(cr, """
+                UPDATE
+                    account_payment
+                SET
+                    journal_id=%s
+                WHERE id in %s
+                """ % (new_journal_id, tuple(wrong_checks.ids)),)
         # wrong_checks.cancel()
         # wrong_checks.unlink()
 
