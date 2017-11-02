0a05be13a6a835fbe6e340dc0e6a31c32e2657dd
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-11-02 15:25:43 -0300

    REF

--- account_check/__openerp__.py
+++ account_check/__openerp__.py
@@ -23,8 +23,7 @@
     'version': '9.0.1.19.0',
     'category': 'Accounting',
     'summary': 'Accounting, Payment, Check, Third, Issue',
-    'author': "OpenERP Team de Localizacion Argentina,"
-    " Odoo Community Association (OCA)",
+    'author': "OpenERP Team de Localizacion Argentina,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'images': [
     ],
