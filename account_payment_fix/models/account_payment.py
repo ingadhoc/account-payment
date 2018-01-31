d1723e610a85b04627bcbd3f6e4bf3ef34d2dcdc
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-01-31 08:55:39 -0300

    IMP transfers, allow to transfer from any journal

--- account_payment_fix/models/account_payment.py
+++ account_payment_fix/models/account_payment.py
@@ -130,7 +130,10 @@ class AccountPayment(models.Model):
         domain = [('type', 'in', ('bank', 'cash'))]
         if self.payment_type == 'inbound':
             domain.append(('at_least_one_inbound', '=', True))
-        else:
+        # Al final dejamos que para transferencias se pueda elegir
+        # cualquier sin importar si tiene outbound o no
+        # else:
+        elif self.payment_type == 'outbound':
             domain.append(('at_least_one_outbound', '=', True))
         return domain
 
