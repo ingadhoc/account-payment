84c8ee2e6f027a11373abb7c7437f89ab86a1e57
Juan José Scarafía <jjs@adhoc.com.ar>
2018-05-18 16:57:56 -0300

    [IMP] account_payment_group migration (#89)
    * [IMP] account_payment_group migration
    
    * [REF]

--- account_payment_group/__manifest__.py
+++ account_payment_group/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment with Multiple methods",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
-    "author": "ADHOC SA,Odoo Community Association (OCA)",
+    "author": "ADHOC SA,",
     "license": "AGPL-3",
     "application": False,
     'installable': True,
