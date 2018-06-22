2b910b27541a644ed13cf9186d95f52988edacec
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-06-22 11:06:57 -0300

    [REF] remove migration script that force currency on payments (Better to disable it)

--- account_payment_group/migrations/9.0.1.31.0/post-migration.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(cr, version):
-    openupgrade.load_data(
-        cr, 'account_payment_group', 'migrations/9.0.1.31.0/mig_data.xml')
