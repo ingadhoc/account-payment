dcc20d50461c93dcdf4f9bacef3f6d5e5af1d34b
Katherine Zaoral <kz@adhoc.com.ar>
2018-09-17 11:12:49 -0300

    [IMP] account_payment_group: Not needed fix payment
    Since already fixed in account_payment_fix module.

--- account_payment_group/models/account_bank_statement_line.py
+++ account_payment_group/models/account_bank_statement_line.py
@@ -30,15 +30,3 @@ class AccountBankStatementLine(models.Model):
             payment_groups.update({'state': 'draft'})
             payment_groups.unlink()
         return res
-
-    def process_reconciliation(self, counterpart_aml_dicts=None,
-                               payment_aml_rec=None, new_aml_dicts=None):
-        """ Pass reconcilation parameters by context in order to
-        capture them in the post() method and be able to get a better
-        partner_id/partner_type interpetration
-        """
-        return super(AccountBankStatementLine, self.with_context(
-            counterpart_aml_dicts=counterpart_aml_dicts,
-            )).process_reconciliation(
-                counterpart_aml_dicts=counterpart_aml_dicts,
-                payment_aml_rec=payment_aml_rec, new_aml_dicts=new_aml_dicts)
