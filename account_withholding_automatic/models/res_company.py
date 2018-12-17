21c52eaaecd999b9da47d88e997f8f88d586786b
Juan Jose Scarafia <jjs@adhoc.com.ar>
2018-12-17 13:10:12 -0300

    [FIX] lint

--- account_withholding_automatic/models/res_company.py
+++ account_withholding_automatic/models/res_company.py
@@ -2,8 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo import models, fields
 
 
 class ResCompany(models.Model):
