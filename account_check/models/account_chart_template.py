d8be4181211f9f11182ad2d0c9a14a53bd5db095
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-04-10 16:25:27 -0300

    Fixes for 12.0

--- account_check/models/account_chart_template.py
+++ account_check/models/account_chart_template.py
@@ -32,13 +32,12 @@ class AccountChartTemplate(models.Model):
 
     @api.multi
     def _load_template(
-            self, company, code_digits=None, transfer_account_id=None,
+            self, company, code_digits=None,
             account_ref=None, taxes_ref=None):
         account_ref, taxes_ref = super(
             AccountChartTemplate, self)._load_template(
                 company,
-                code_digits=code_digits,
-                transfer_account_id=transfer_account_id,
+                code_digits=code_digits,                
                 account_ref=account_ref,
                 taxes_ref=taxes_ref)
         for field in [
@@ -52,12 +51,8 @@ class AccountChartTemplate(models.Model):
                 company[field] = account_ref[account_field.id]
         return account_ref, taxes_ref
 
-
-class WizardMultiChartsAccounts(models.TransientModel):
-    _inherit = 'wizard.multi.charts.accounts'
-
     @api.multi
-    def _create_bank_journals_from_o2m(self, company, acc_template_ref):
+    def _create_bank_journals(self, company, acc_template_ref):
         """
         Bank - Cash journals are created with this method
         Inherit this function in order to add checks to cash and bank
@@ -65,6 +60,42 @@ class WizardMultiChartsAccounts(models.TransientModel):
         and they will be disable by default
         """
 
+        res = super(
+            AccountChartTemplate, self)._create_bank_journals(
+            company, acc_template_ref)
+
+        # creamos diario para cheques de terceros
+        received_third_check = self.env.ref(
+            'account_check.account_payment_method_received_third_check')
+        delivered_third_check = self.env.ref(
+            'account_check.account_payment_method_delivered_third_check')
+        self.env['account.journal'].create({
+            'name': 'Cheques de Terceros',
+            'type': 'cash',
+            'company_id': company.id,
+            'inbound_payment_method_ids': [
+                (4, received_third_check.id, None)],
+            'outbound_payment_method_ids': [
+                (4, delivered_third_check.id, None)],
+        })
+
+        self.env['account.journal'].with_context(
+            force_company_id=company.id)._enable_issue_check_on_bank_journals()
+        return res
+
+"""
+class WizardMultiChartsAccounts(models.TransientModel):
+    _inherit = 'wizard.multi.charts.accounts'
+
+    @api.multi
+    def _create_bank_journals_from_o2m(self, company, acc_template_ref):
+        """
+"""        Bank - Cash journals are created with this method
+        Inherit this function in order to add checks to cash and bank
+        journals. This is because usually will be installed before chart loaded
+        and they will be disable by default
+        """
+"""
         res = super(
             WizardMultiChartsAccounts, self)._create_bank_journals_from_o2m(
             company, acc_template_ref)
@@ -87,3 +118,4 @@ class WizardMultiChartsAccounts(models.TransientModel):
         self.env['account.journal'].with_context(
             force_company_id=company.id)._enable_issue_check_on_bank_journals()
         return res
+"""
--- account_withholding/models/account_chart_template.py
+++ account_withholding/models/account_chart_template.py
@@ -7,11 +7,10 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class WizardMultiChartsAccounts(models.TransientModel):
-    _inherit = 'wizard.multi.charts.accounts'
+class AccountChartTemplate(models.Model):
+    _inherit = "account.chart.template"
 
-    @api.multi
-    def _create_bank_journals_from_o2m(self, company, acc_template_ref):
+    def _create_bank_journals(self, company, acc_template_ref):
         """
         Bank - Cash journals are created with this method
         Inherit this function in order to add checks to cash and bank
@@ -19,6 +18,49 @@ class WizardMultiChartsAccounts(models.TransientModel):
         and they will be disable by default
         """
         res = super(
+            AccountChartTemplate, self)._create_bank_journals(
+            company, acc_template_ref)
+
+        # each chart of account / localization should send this key if
+        # they want withholding journal to be created
+        if self._context.get('create_withholding_journal'):
+            # creamos diario para retenciones
+            inbound_withholding = self.env.ref(
+                'account_withholding.account_payment_method_in_withholding')
+            outbound_withholding = self.env.ref(
+                'account_withholding.account_payment_method_out_withholding')
+            journal = self.env['account.journal'].create({
+                'name': 'Retenciones',
+                'type': 'cash',
+                'company_id': company.id,
+                'inbound_payment_method_ids': [
+                    (4, inbound_withholding.id, None)],
+                'outbound_payment_method_ids': [
+                    (4, outbound_withholding.id, None)],
+            })
+            # we dont want this journal to have accounts and we can not inherit
+            # to avoid creation, so we delete it
+            journal.default_credit_account_id.with_context(
+                force_unlink=True).unlink()
+
+        return res
+
+AccountChartTemplate()
+
+
+"""
+class WizardMultiChartsAccounts(models.TransientModel):
+    _inherit = 'wizard.multi.charts.accounts'
+
+    @api.multi
+    def _create_bank_journals_from_o2m(self, company, acc_template_ref):
+        """
+"""        Bank - Cash journals are created with this method
+        Inherit this function in order to add checks to cash and bank
+        journals. This is because usually will be installed before chart loaded
+        and they will be disable by default
+        """
+"""        res = super(
             WizardMultiChartsAccounts, self)._create_bank_journals_from_o2m(
             company, acc_template_ref)
 
@@ -45,3 +87,4 @@ class WizardMultiChartsAccounts(models.TransientModel):
                 force_unlink=True).unlink()
 
         return res
+"""
