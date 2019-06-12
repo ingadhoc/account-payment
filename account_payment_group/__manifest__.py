068b2db8e47f60dad29646f1261e2a86b7b6812c
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-06-11 23:05:34 -0300

    [REF]

--- account_payment_group/__manifest__.py
+++ account_payment_group/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment with Multiple methods",
-    "version": "12.0.1.9.0",
+    "version": "12.0.1.0.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA,",
--- account_payment_group_document/__manifest__.py
+++ account_payment_group_document/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Payment Groups with Accounting Documents",
-    "version": "11.0.1.1.0",
+    "version": "12.0.1.0.0",
     "author": "ADHOC SA,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Accounting",
