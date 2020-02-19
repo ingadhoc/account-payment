8815f792f8bebdb553e9e2b7d3a71cfe2ab632a5
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-02-19 09:00:18 -0300

    [FIX]

--- account_check/models/account_move.py
+++ account_check/models/account_move.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class AccountMove(models.Model):
@@ -17,31 +17,31 @@ class AccountMove(models.Model):
         'Rejected Check',
     )
 
-    def action_cancel(self):
+    def button_draft(self):
         """
         Si al cancelar la factura la misma estaba vinculada a un rechazo
         intentamos romper la conciliacion del rechazo
         """
-        for rec in self.filtered(lambda x: x.rejected_check_id):
+        for rec in self.filtered('rejected_check_id'):
             check = rec.rejected_check_id
             deferred_account = check.company_id._get_check_account('deferred')
             if (
                     check.state == 'rejected' and
                     check.type == 'issue_check' and
                     deferred_account.reconcile):
-                deferred_account_line = rec.move_id.line_ids.filtered(
+                deferred_account_line = rec.line_ids.filtered(
                     lambda x: x.account_id == deferred_account)
                 deferred_account_line.remove_move_reconcile()
-        return super().action_cancel()
+        return super().button_cancel()
 
-    def action_move_create(self):
+    def post(self):
         """
         Si al validar la factura, la misma tiene un cheque de rechazo asociado
         intentamos concilarlo
         """
-        res = super(AccountInvoice, self).action_move_create()
-        for rec in self.filtered(lambda x: x.rejected_check_id):
+        res = super().post()
+        for rec in self.filtered('rejected_check_id'):
             check = rec.rejected_check_id
             if check.state == 'rejected' and check.type == 'issue_check':
-                rec.rejected_check_id.handed_reconcile(rec.move_id)
+                rec.rejected_check_id.handed_reconcile(rec)
         return res
