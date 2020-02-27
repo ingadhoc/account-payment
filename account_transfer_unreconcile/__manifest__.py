4f2d8677b8fb6e38dedce3e543bda2d90fa48620
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-02-27 12:04:15 -0300

    [MIG] account_transfer_unreconcile: Migration to 13.0

--- account_transfer_unreconcile/__manifest__.py
+++ account_transfer_unreconcile/__manifest__.py
@@ -2,19 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Unreconcile Moves on Transfer Cancellation",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    'installable': False,
+    'installable': True,
     "external_dependencies": {
         "python": [],
         "bin": [],
     },
     "depends": [
-        "account_cancel",
+        "account",
     ],
     "data": [
     ],
