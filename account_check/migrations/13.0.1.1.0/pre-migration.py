af75435ddec79f9395bc302aa2aa2404c7d54447
katialogas <katia.logas@gmail.com>
2020-08-24 12:54:24 -0300

    Cheques: Actualizar carpeta migrate para que elimine columna issue_check_subtype

--- account_check/migrations/13.0.1.1.0/pre-migration.py
+++ account_check/migrations/13.0.1.1.0/pre-migration.py
@@ -6,6 +6,7 @@ _logger = logging.getLogger(__name__)
 def create_column_table(cr):
     cr.execute("""ALTER TABLE account_checkbook ADD COLUMN check_subtype character varying""")
     cr.execute("""UPDATE account_checkbook SET check_subtype=issue_check_subtype""")
+    cr.execute("""ALTER TABLE account_checkbook DROP COLUMN issue_check_subtype""")
 
 
 def migrate(cr, version):
