337b35b940ac39cebca2d630108fc99120dc2f11
Pablo Paez <pablopaezps97@gmail.com>
2021-02-02 16:22:05 -0300

    [FIX]account_check: add payment_date field on check_view

--- account_check/__manifest__.py
+++ account_check/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Check Management',
-    'version': "13.0.1.2.0",
+    'version': "13.0.1.3.0",
     'category': 'Accounting',
     'summary': 'Accounting, Payment, Check, Third, Issue',
     'author': 'ADHOC SA, AITIC S.A.S',
