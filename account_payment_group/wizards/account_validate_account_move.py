c2418a8273aa0d26766f38bb2b1f47c8bb06e1c8
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-03-30 09:18:42 -0300

    [13.0] [IMP] account_payment_group: Adapt wizard "massive validation invoices" for pay now validation.
    Ticket Nbr:38001

---
+++ account_payment_group/wizards/account_validate_account_move.py
@@ -0,0 +1,20 @@
+from odoo import models, _
+from odoo.exceptions import UserError
+
+
+class ValidateAccountMove(models.TransientModel):
+    _inherit = "validate.account.move"
+
+    def validate_move(self):
+        if self._context.get('active_model') == 'account.move':
+            domain = [('id', 'in', self._context.get('active_ids', [])), ('state', '=', 'draft')]
+        elif self._context.get('active_model') == 'account.journal':
+            domain = [('journal_id', '=', self._context.get('active_id')), ('state', '=', 'draft')]
+        else:
+            raise UserError(_("Missing 'active_model' in context."))
+
+        moves = self.env['account.move'].search(domain).filtered('line_ids')
+        res = super().validate_move()
+        # we try to pay automatic if the pay now journal is setting on the invoice.
+        moves.pay_now()
+        return res
