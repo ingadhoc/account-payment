69c328f2b6d535abdca20908b8d3d14d0df522f6
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-04-13 14:50:36 -0300

    [13.0] [FIX] account_payment_group: Fix a bug when post payments.
    This changes here https://github.com/ingadhoc/account-payment/commit/6f87f0610a810ba661ce3b1f18a7e1743b599090
    It's cause problems with the concilation process in v13.

--- account_payment_group/models/account_payment_group.py
+++ account_payment_group/models/account_payment_group.py
@@ -578,7 +578,7 @@ class AccountPaymentGroup(models.Model):
             # al crear desde website odoo crea primero el pago y lo postea
             # y no debemos re-postearlo
             if not create_from_website and not create_from_expense:
-                rec.payment_ids.sorted(key=lambda l: l.signed_amount).filtered(lambda x: x.state == 'draft').post()
+                rec.payment_ids.filtered(lambda x: x.state == 'draft').post()
 
             counterpart_aml = rec.payment_ids.mapped('move_line_ids').filtered(
                 lambda r: not r.reconciled and r.account_id.internal_type in (
