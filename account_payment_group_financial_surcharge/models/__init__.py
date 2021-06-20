4f9bacc75adda92ae27f66003e02692f8caeb9a4
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-06-20 06:45:58 -0300

    [IMP] surcharge: views, models. use surcharge approach

--- account_payment_group_financial_surcharge/models/__init__.py
+++ account_payment_group_financial_surcharge/models/__init__.py
@@ -1,4 +1,5 @@
 from . import account_financing_plans
 from . import account_journal
 from . import account_payment
+from . import account_payment_group
 from . import res_company
--- account_payment_group_financial_surcharge/models/account_financing_plans.py
+++ account_payment_group_financial_surcharge/models/account_financing_plans.py
@@ -8,7 +8,12 @@ from odoo import models, fields
 class AccountFinancingPlans(models.Model):
     _name = 'account.financing_plans'
     _description = 'account.financing_plans'
+    _order = 'sequence asc'
 
+    sequence = fields.Integer(
+        'Sequence',
+        default=10,
+    )
     name = fields.Char(required=True)
     surcharge_coefficient = fields.Integer(
         'Surcharge coefficient',
