c2dc798a245e1e981f31dbf18203b2b9c8775cc5
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-06-20 07:02:12 -0300

    [IMP] surcharge: use net amount approach

--- account_payment_group_financial_surcharge/models/account_payment.py
+++ account_payment_group_financial_surcharge/models/account_payment.py
@@ -12,7 +12,19 @@ class AccountPayment(models.Model):
     available_financing_plans_ids = fields.Many2many(related='journal_id.financing_plans_ids')
     financing_plan_id = fields.Many2one(
         'account.financing_plans', store=True, readonly=False, compute='_compute_financing_plan')
-    surcharged_amount = fields.Monetary(compute='_computed_surcharged_amount')
+    net_amount = fields.Monetary(compute='_computed_net_amount', inverse='_inverse_net_amount')
+
+    @api.depends('amount')
+    def _computed_net_amount(self):
+        surcharged_payments = self.filtered('financing_plan_id')
+        (self - surcharged_payments).net_amount = False
+        for rec in surcharged_payments:
+            rec.net_amount = rec.amount * (1 - rec.financing_plan_id.surcharge_coefficient / 100.0)
+
+    @api.onchange('net_amount', 'financing_plan_id')
+    def _inverse_net_amount(self):
+        for rec in self:
+            rec.amount = rec.net_amount / (1 - rec.financing_plan_id.surcharge_coefficient / 100.0)
 
     @api.depends('available_financing_plans_ids', 'payment_type')
     def _compute_financing_plan(self):
@@ -20,10 +32,3 @@ class AccountPayment(models.Model):
         (self - with_plan).financing_plan_id = False
         for rec in with_plan:
             rec.financing_plan_id = rec._origin.available_financing_plans_ids[0]
-
-    @api.depends('amount', 'financing_plan_id')
-    def _computed_surcharged_amount(self):
-        surcharged_payments = self.filtered('financing_plan_id')
-        (self - surcharged_payments).surcharged_amount = False
-        for rec in surcharged_payments:
-            rec.surcharged_amount = rec.amount / (1 - rec.financing_plan_id.surcharge_coefficient / 100.0)
