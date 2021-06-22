1f1efb2be9a5bfa7446f9cb92e28e714d648d09a
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-06-22 11:15:57 -0300

    [IMP] add when validate payment with surcharge to create an concile the corresponding debit note.

--- account_payment_group_financial_surcharge/models/account_payment_group.py
+++ account_payment_group_financial_surcharge/models/account_payment_group.py
@@ -3,7 +3,7 @@
 # directory
 ##############################################################################
 from odoo import models, fields, api
-
+from odoo.exceptions import UserError
 
 class AccountPaymentGroup(models.Model):
 
@@ -16,3 +16,21 @@ class AccountPaymentGroup(models.Model):
         for rec in self:
             rec.financing_surcharge = sum(
                 rec.payment_ids.filtered('financing_plan_id').mapped(lambda x: x.amount - x.net_amount))
+
+    def post(self):
+        if self.payment_ids.mapped('financing_plan_id'):
+            if not self.company_id.product_surcharge_id:
+                raise UserError(
+                    "To validate payment with finacing plan is necessary to have a product surcharge in the "
+                    "company of the payment. Please check this in the Account Config")
+            journal = self.env['account.journal'].search([
+                ('type', '=', 'sale'),
+                ('company_id', '=', self.company_id.id)], limit=1)
+            wiz = self.env['account.payment.group.invoice.wizard'].with_context(active_id=self.id, internal_type='debit_note').create({
+                'journal_id': journal.id,
+                'product_id': self.company_id.product_surcharge_id.id,
+                'amount_total': self.financing_surcharge,
+                })
+            wiz.change_payment_group()
+            wiz.confirm()
+        super().post()
