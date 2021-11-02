301e1e1bf41e7e4153cd73e547a17e7951438256
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2021-11-02 14:36:28 -0300

    [13.0] [FIX] account_payment_group: Avoid unpaid invoices when an error ocurrs in validation process (#252)
    Co-authored-by: Katherine Zaoral <zaoral@users.noreply.github.com>

--- account_payment_group/wizards/account_validate_account_move.py
+++ account_payment_group/wizards/account_validate_account_move.py
@@ -14,7 +14,16 @@ class ValidateAccountMove(models.TransientModel):
             raise UserError(_("Missing 'active_model' in context."))
 
         moves = self.env['account.move'].search(domain).filtered('line_ids')
-        res = super().validate_move()
-        # we try to pay automatic if the pay now journal is setting on the invoice.
+
+        try:
+            res = super().validate_move()
+        except UserError as error:
+            # we consider that an error with "Afip" occurred and we need to pay the invoice with pay now
+            if 'AFIP' in repr(error):
+                # we try to pay automatic if the pay now journal is setting on the invoice.
+                moves.pay_now()
+                if not self.env.context.get('l10n_ar_invoice_skip_commit'):
+                    self._cr.commit()
+            raise UserError(error)
         moves.pay_now()
         return res
