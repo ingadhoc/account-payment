a513cda766802f25ed16bc90f1482dd78fd0f651
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-10-24 16:10:51 -0300

    [REM] _payment_group: constraint that blocks pay to company Esta restriccion, tal como figuraba en sus comentarios, ya no es necesaria

--- account_payment_group/models/account_payment_group.py
+++ account_payment_group/models/account_payment_group.py
@@ -511,16 +511,6 @@ class AccountPaymentGroup(models.Model):
                 raise ValidationError(_('Payment group for partner %s but payment lines are of partner %s') % (
                     rec.partner_id.name, to_pay_partners.name))
 
-    @api.constrains('partner_id', 'company_id')
-    def _check_no_transfer(self):
-        # TODO en realidad si habría casos de uso donde esto es necesario se podría permitir sin problemas,
-        # de hecho odoo hizo un cambio para permitirlo acá
-        # https://github.com/odoo/odoo/commit/362d8cbf7724431672b8b73fb5f4682d4d2c3f66
-        # igual por el momento parece ser más apropiado recomendar transferencia interna
-        transfers = self.filtered(lambda x: x.company_id.partner_id == x.partner_id)
-        if transfers:
-            raise ValidationError(_("You can't make a payment/receipt to your same company, create an internal transfer instead"))
-
     # from old account_payment_document_number
 
     @api.depends('name')
