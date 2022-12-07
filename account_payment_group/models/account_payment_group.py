c3c19af7cd5e5b2bd9074d1852ea9d6cc0a9929a
Pablo Montenegro <pam@adhoc.com.ar>
2022-12-07 12:36:11 -0300

    [FIX]account_payment_group:order lines by date maturity Ticket: 57884

--- account_payment_group/models/account_payment_group.py
+++ account_payment_group/models/account_payment_group.py
@@ -363,7 +363,13 @@ class AccountPaymentGroup(models.Model):
         """
         for rec in self:
             payment_lines = rec.payment_ids.mapped('move_line_ids').filtered(lambda x: x.account_internal_type in ['receivable', 'payable'])
-            rec.matched_move_line_ids =  (payment_lines.mapped('matched_debit_ids.debit_move_id').sorted(key=lambda x: x.date_maturity) | payment_lines.mapped('matched_credit_ids.credit_move_id')).sorted(key=lambda x: x.date_maturity) - payment_lines
+            debit_moves = payment_lines.mapped('matched_debit_ids.debit_move_id')
+            credit_moves = payment_lines.mapped('matched_credit_ids.credit_move_id')
+            debit_lines_sorted = debit_moves.filtered(lambda x: x.date_maturity != False).sorted(key=lambda x: x.date_maturity)
+            credit_lines_sorted = credit_moves.filtered(lambda x: x.date_maturity != False).sorted(key=lambda x: x.date_maturity)
+            debit_lines_without_date_maturity = debit_moves - debit_lines_sorted
+            credit_lines_without_date_maturity = credit_moves - credit_lines_sorted
+            rec.matched_move_line_ids =  ((debit_lines_sorted + debit_lines_without_date_maturity) | (credit_lines_sorted + credit_lines_without_date_maturity)) - payment_lines
 
     @api.depends('payment_ids.move_line_ids')
     def _compute_move_lines(self):
