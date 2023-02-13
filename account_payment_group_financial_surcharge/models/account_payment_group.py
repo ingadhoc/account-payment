7035419e9644f401c1d9423fde342b14131208f7
Katherine Zaoral <kz@adhoc.com.ar>
2023-02-13 12:11:28 -0300

    [ADD] account_payment_group_financial_surcharge: Avoid ND = 0
    If the financial plan does not add any subcharge we should not
    create a ND amount 0. Only create the ND when we have a subcharge to
    apply
    
    Original commit: 546415cd8111d7e3a9e257a517504f909903979a

--- account_payment_group_financial_surcharge/models/account_payment_group.py
+++ account_payment_group_financial_surcharge/models/account_payment_group.py
@@ -19,7 +19,7 @@ class AccountPaymentGroup(models.Model):
             rec.financing_surcharge = sum(rec.payment_ids.filtered('installment_id').mapped(lambda x: x.amount - x.net_amount))
 
     def post(self):
-        if self.financing_surcharge:
+        if self.filtered(lambda p: p.financing_surcharge > 0):
             product = self.company_id.product_surcharge_id
             if not product:
                 raise UserError(
