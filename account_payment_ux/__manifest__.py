0b06463dd2c0a9c2ee74be378c5dddeadb0375b4
Martin Quinteros <maq@adhoc.com.ar>
2023-07-12 08:50:41 -0300

    [ADD] Add new module accout_payment_ux Fix the behavior of pending transacition realated with a account.payment 	What is happening now: 		- when i'm create a electronic payment from backend and the transaction remains in pending status the payment status is changed to canceled 	what happens after installing this module 		- When the transaction is pending the payment remains in draft status. After done this transaction the payment change its state ans reconcilie with the invoices

---
+++ account_payment_ux/__manifest__.py
@@ -0,0 +1,24 @@
+# © 2023 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Payment UX",
+    "version": "16.0.1.0.0",
+    "category": "Payment",
+    "website": "www.adhoc.com.ar",
+    "author": "ADHOC SA",
+    "license": "AGPL-3",
+    "application": False,
+    'installable': True,
+    'auto_install': True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account_payment",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+}
---
+++ account_payment_ux/models/__init__.py
@@ -0,0 +1,2 @@
+from . import account_payment
+from . import payment_transaction
