585d64482c46bd347fe34e4a056285414b305406
Martin Quinteros <maq@adhoc.com.ar>
2023-11-10 08:48:43 -0300

    [ADD] add new module account_cashbox_l10n_latam_check

---
+++ account_cashbox_l10n_latam_check/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Check Cashbox management",
+    "summary": "Add cashbox for check operations",
+    "version": "16.0.1.0.1",
+    "category": "Accounting",
+    "website": "www.adhoc.com.ar",
+    "author": "ADHOC SA",
+    "license": "AGPL-3",
+    "depends": [
+        "account_cashbox",
+        "l10n_latam_check",
+        ],
+    "demo": [],
+    "data": [
+        'wizards/l10n_latam_payment_mass_transfer_views.xml',
+        ],
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+}
