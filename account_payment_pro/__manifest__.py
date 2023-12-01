0f9e31da12b4260c3606064a221a484c406fab25
Juan José Scarafia <jjs@adhoc.com.ar>
2023-12-01 20:49:46 +0000

    [ADD] account_payment_pro
    Part-of: ingadhoc/account-payment#424

---
+++ account_payment_pro/__manifest__.py
@@ -0,0 +1,27 @@
+# © 2023 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Payment Super Power",
+    "version": "17.0.1.0.0",
+    "category": "Payment",
+    "website": "www.adhoc.com.ar",
+    "author": "ADHOC SA",
+    "license": "AGPL-3",
+    'installable': True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account",
+        # TODO mover esto a modulo puente
+        "l10n_latam_invoice_document",
+    ],
+    "data": [
+        'wizards/account_payment_invoice_wizard_view.xml',
+        'views/account_payment_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    "demo": [
+    ],
+}
---
+++ account_payment_pro/wizards/__init__.py
@@ -0,0 +1,5 @@
+# © 2016 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import account_payment_invoice_wizard
+# from . import res_config_settings
