2b77accd76c6b353934b035700b5f9df9cc71687
Martin Quinteros <maq@adhoc.com.ar>
2023-12-01 14:09:49 +0000

    [REM] Remove Payment Group module: Deprecated for 17.0
    closes ingadhoc/account-payment#422
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- account_payment_group/migrations/13.0.1.5.0/post-migration.py
+++
@@ -1,8 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.load_data(
-        env.cr, 'account_payment_group',
-        'migrations/13.0.1.5.0/mig_data.xml')
