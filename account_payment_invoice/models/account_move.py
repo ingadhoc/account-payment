0928948958f0d52eacf6765ab840314a0ed3c6b2
Martin Quinteros <maq@adhoc.com.ar>
2024-01-12 16:51:04 -0300

    [FIX] add translation mtehod in error
    X-original-commit: d8631d41df9364f8931cb9df46ec4f42691ad532

--- account_payment_invoice/models/account_move.py
+++ account_payment_invoice/models/account_move.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.tools import plaintext2html
 
 
