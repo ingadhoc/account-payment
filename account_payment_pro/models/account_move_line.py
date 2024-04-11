f7f60f62d8bfcc127bdc4393ed097f0c8f46e9a8
Martin Quinteros <maq@adhoc.com.ar>
2024-04-11 09:19:03 -0300

    [IMP] account_payment_pro: re add matched_move_line_ids in payment

--- account_payment_pro/models/account_move_line.py
+++ account_payment_pro/models/account_move_line.py
@@ -10,18 +10,18 @@ class AccountMoveLine(models.Model):
         currency_field='company_currency_id',
     )
 
-    @api.depends_context('matched_payment_id')
+    @api.depends_context('matched_payment_ids')
     def _compute_payment_matched_amount(self):
         """
         Reciviendo un matched_payment_id por contexto, decimos en ese payment, cuanto se pago para la lína en cuestión.
         """
-        matched_payment_id = self._context.get('matched_payment_id', self._context.get('params', {}).get('id'))
+        matched_payment_ids = self._context.get('matched_payment_ids')
 
-        if not matched_payment_id:
+        if not matched_payment_ids:
             self.payment_matched_amount = 0.0
             return False
-        payments = self.env['account.payment'].browse(matched_payment_id)
-        payment_lines = payments.line_ids.filtered(lambda x: x.account_type in ['asset_receivable', 'liability_payable'])
+        payments = self.env['account.payment'].search([('id', 'in', matched_payment_ids)])
+        payment_lines = payments.mapped('line_ids').filtered(lambda x: x.account_type in ['asset_receivable', 'liability_payable'])
         for rec in self:
             debit_move_amount = sum(payment_lines.mapped('matched_debit_ids').filtered(lambda x: x.debit_move_id == rec).mapped('amount'))
             credit_move_amount = sum(payment_lines.mapped('matched_credit_ids').filtered(lambda x: x.credit_move_id == rec).mapped('amount'))
