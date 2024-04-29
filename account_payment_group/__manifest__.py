cffd950dcc53db73ab9634d31cec15d2d9048c96
Rocío Vega <rov@adhoc.com.ar>
2024-04-29 12:34:35 -0300

    [FIX] account_payment_group: fix in manifest because the pr #477

--- account_payment_group/__manifest__.py
+++ account_payment_group/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment with Multiple methods",
-    "version": "16.0.1.10.0",
+    "version": "16.0.1.11.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA, AITIC S.A.S",
