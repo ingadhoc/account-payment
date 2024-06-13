2c69b8fc0e9b8564191d6581880047ee20b2704b
“Virginia <vib@somosadhoc.com>
2024-06-13 16:56:08 +0000

    [FIX] account_payment_pro_receiptbook: add subtype_xmlid to message_post_with_source
    closes ingadhoc/account-payment#493
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

--- account_payment_pro_receiptbook/models/account_payment.py
+++ account_payment_pro_receiptbook/models/account_payment.py
@@ -31,7 +31,11 @@ class AccountPayment(models.Model):
         res = super().action_post()
 
         for rec in self.filtered('receiptbook_id.mail_template_id'):
-            rec.message_post_with_source(rec.receiptbook_id.mail_template_id)
+            rec.message_post_with_source(
+                rec.receiptbook_id.mail_template_id,
+                message_type='comment',
+                subtype_xmlid='mail.mt_comment'
+            )
         return res
 
     @api.depends('company_id', 'partner_type', 'is_internal_transfer')
