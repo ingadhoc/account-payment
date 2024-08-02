d1843ec016f1a31d2d4b13faa4bd880f341210fb
Felipe Garcia Suez <feg@adhoc.com.ar>
2024-08-02 15:31:09 -0300

    [ADD] account_payment_pro_receiptbook: Added multi-company rule

--- account_payment_pro_receiptbook/__manifest__.py
+++ account_payment_pro_receiptbook/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment receiptbook",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.2.0",
     "category": "Payment",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
@@ -18,6 +18,7 @@
     ],
     "data": [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/account_payment_receipt_group.xml',
         'views/account_payment.xml',
         'data/l10n_latam.document.type.csv',
