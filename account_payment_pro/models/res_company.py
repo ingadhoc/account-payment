7a6c13de5bdead18a2db3c77de5a5f19dfe024ab
Juan José Scarafia <jjs@adhoc.com.ar>
2024-08-08 00:00:08 +0000

    [REF] pay_pro: do not compute amount on onchange
    Part-of: ingadhoc/account-payment#515

--- account_payment_pro/models/res_company.py
+++ account_payment_pro/models/res_company.py
@@ -10,11 +10,8 @@ class ResCompany(models.Model):
     )
     use_payment_pro = fields.Boolean(compute='_compute_use_payment_pro', store=True, readonly=False)
 
-    
     @api.depends('partner_id.country_id.code')
     def _compute_use_payment_pro(self):
         ar_companies = self.filtered(lambda x: x.partner_id.country_id.code == 'AR')
         ar_companies.use_payment_pro = True
         (self - ar_companies).use_payment_pro = False
-
-        
