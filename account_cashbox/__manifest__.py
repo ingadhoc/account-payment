2115a80736c8b4f6413ecf528d9e228c39158981
Rocío Vega <rov@adhoc.com.ar>
2024-09-19 19:52:21 +0000

    [FIX] account_cashbox: Remove obsolete unique constraint from account_cashbox_session due to new constraint added in https://github.com/ingadhoc/account-payment/commit/db574bf536e815d10221859924381e4a3a86af4d#diff-fd2084468cbef678a2cb52e2257b09797d716c4ef6031d237b29f544ff6c0d77L40
    closes ingadhoc/account-payment#531
    
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>

--- account_cashbox/__manifest__.py
+++ account_cashbox/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Cashbox management",
     "summary": "Introduces concept cashbox and accounting journal sessions",
-    "version": "16.0.1.4.0",
+    "version": "16.0.1.5.0",
     "category": "Accounting",
     "website": "www.adhoc.com.ar",
     "author": "juanpgarza, ADHOC SA",
