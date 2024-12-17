a2fb39ffe90361506689d3a252375676a2559e5e
Camila Vives <cav@adhoc.com.ar>
2024-12-17 13:00:04 +0000

    [FIX] account_payment_pro: reestablish pay now settings
    X-original-commit: cfd2bd60b232ee3ba66a8f2b86bf3c68a108cd24

--- account_payment_pro/models/res_config_setting.py
+++ account_payment_pro/models/res_config_setting.py
@@ -8,3 +8,15 @@ class ResConfigSettings(models.TransientModel):
         related='company_id.use_payment_pro',
         readonly=False,
     )
+
+    group_pay_now_customer_invoices = fields.Boolean(
+        'Allow pay now on customer invoices?',
+        implied_group='account_payment_pro.group_pay_now_customer_invoices',
+    )
+    group_pay_now_vendor_invoices = fields.Boolean(
+        'Allow pay now on vendor invoices?',
+        help='Allow users to choose a payment journal on invoices so that '
+        'invoice is automatically paid after invoice validation. A payment '
+        'will be created using choosen journal',
+        implied_group='account_payment_pro.group_pay_now_vendor_invoices',
+    )
