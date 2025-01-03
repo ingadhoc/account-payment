4741f843f81d2ff3bc15fdadb16036c0d6652349
Camila Vives <cav@adhoc.com.ar>
2025-01-03 09:42:39 -0300

    [FIX] account_payment_financial_surcharge: fix double pay buttons

--- account_payment_financial_surcharge/__manifest__.py
+++ account_payment_financial_surcharge/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Payments with Financial Surchange",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "author": "ADHOC SA",
     "license": "AGPL-3",
     "category": "Payment",
