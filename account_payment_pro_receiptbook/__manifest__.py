ebfbad76267aa399ccdb6a7da2f4074c83f2d4b8
Rocío Vega <rov@adhoc.com.ar>
2025-01-27 15:06:51 +0000

    [IMP] account_payment_pro_receiptbook: Make auto installable if account_payment_pro and l10n_ar_account_tax are installed
    X-original-commit: 8d1d07962fc3899715188bcf05206a2a88a1bd7e

--- account_payment_pro_receiptbook/__manifest__.py
+++ account_payment_pro_receiptbook/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment receiptbook",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Payment",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
@@ -23,7 +23,7 @@
         'data/l10n_latam.document.type.csv',
     ],
     'installable': True,
-    'auto_install': False,
+    'auto_install': ["account_payment_pro","l10n_ar_account_tax"],
     'application': False,
     'post_init_hook': '_generate_receiptbooks',
     "demo": [
