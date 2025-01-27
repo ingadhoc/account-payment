aa56a0e9bfd56eb655fec3fa4fbf568d54f0c8ad
Rocío Vega <rov@adhoc.com.ar>
2025-01-27 18:36:12 +0000

    [FIX] account_payment_pro: Fix when calculate unmatched_amount
    This error was introducing in the following commit https://github.com/ingadhoc/account-payment/pull/587
    
    This commit tried to solve the problem when paying with a different currency of the bill.
    Only in that case the payment_total was consider with an inconsistent sign.
    
    This commit consider both cases.
    
    closes ingadhoc/account-payment#603
    
    Signed-off-by: Camila Vives <cav@adhoc.com.ar>
    X-original-commit: 7e67a59567307de478365033184d419fdf5eb621

--- account_payment_pro/models/account_payment.py
+++ account_payment_pro/models/account_payment.py
@@ -414,7 +414,7 @@ class AccountPayment(models.Model):
             sign = rec.partner_type == 'supplier' and -1.0 or 1.0
             rec.matched_amount = sign * sum(
                 rec.matched_move_line_ids.with_context(matched_payment_ids=rec.ids).mapped('payment_matched_amount'))
-            rec.unmatched_amount = sign * rec.payment_total - rec.matched_amount
+            rec.unmatched_amount = abs(rec.payment_total) - rec.matched_amount
 
     @api.depends('to_pay_move_line_ids')
     def _compute_has_outstanding(self):
