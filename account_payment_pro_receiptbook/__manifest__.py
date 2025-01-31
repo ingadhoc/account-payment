f141f3ba711c9c5bd802999798b203622aa86144
Rocío Vega <rov@adhoc.com.ar>
2025-01-31 19:09:05 +0000

    [FIX] account_payment_pro_receiptbook: Fix the auto install dependencies
    X-original-commit: 05f57f926bdc33ce1e7cbe2cb317de4872f29860

--- account_payment_pro_receiptbook/__manifest__.py
+++ account_payment_pro_receiptbook/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment receiptbook",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "category": "Payment",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
@@ -23,7 +23,7 @@
         'data/l10n_latam.document.type.csv',
     ],
     'installable': True,
-    'auto_install': ["account_payment_pro","l10n_ar_account_tax"],
+    'auto_install': ["account_payment_pro","l10n_ar_account_tax_settlement"],
     'application': False,
     'post_init_hook': '_generate_receiptbooks',
     "demo": [
