80db5f453760d50b2ae6715b292cc4a8f5e16dd0
Martin Quinteros <maq@adhoc.com.ar>
2025-02-04 21:45:35 +0000

    [REV] revert commit https://github.com/ingadhoc/account-payment/commit/05f57f926bdc33ce1e7cbe2cb317de4872f29860
    X-original-commit: 14be1fd2b782ec8a53bb7211d3ea8a1b58d08231

--- account_payment_pro_receiptbook/__manifest__.py
+++ account_payment_pro_receiptbook/__manifest__.py
@@ -2,7 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment receiptbook",
+<<<<<<< HEAD
     "version": "18.0.1.2.0",
+||||||| parent of da0d1971 (temp)
+    "version": "17.0.1.5.0",
+=======
+    "version": "17.0.1.6.0",
+>>>>>>> da0d1971 (temp)
     "category": "Payment",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
@@ -23,7 +29,13 @@
         'data/l10n_latam.document.type.csv',
     ],
     'installable': True,
+<<<<<<< HEAD
     'auto_install': ["account_payment_pro"],
+||||||| parent of da0d1971 (temp)
+    'auto_install': ["account_payment_pro","l10n_ar_account_tax_settlement"],
+=======
+    'auto_install': False,
+>>>>>>> da0d1971 (temp)
     'application': False,
     'post_init_hook': '_generate_receiptbooks',
     "demo": [
