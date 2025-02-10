1ad74b973a1f5a38a681bbb6907a841140cfdcaa
Rocío Vega <rov@adhoc.com.ar>
2025-02-10 19:42:13 +0000

    [FIX] account_payment_pro_receiptbook: Fix to not set receipbook_id in payments when company do not use payment pro
    X-original-commit: 18d26493eec52ff84f8bf2f6a0f4b76db2d3effa

--- account_payment_pro/models/res_company.py
+++ account_payment_pro/models/res_company.py
@@ -6,7 +6,7 @@ class ResCompany(models.Model):
 
     use_payment_pro = fields.Boolean(compute="_compute_use_payment_pro", store=True, readonly=False)
 
-    @api.depends("partner_id.country_id.code")
+    @api.depends("partner_id.country_id")
     def _compute_use_payment_pro(self):
         ar_companies = self.filtered(lambda x: x.partner_id.country_id.code == "AR")
         ar_companies.use_payment_pro = True
