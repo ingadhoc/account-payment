66933db34a814b3dfeb610cb1e37e53657655437
Rocío Vega <rov@adhoc.com.ar>
2025-04-30 14:45:47 -0300

    [IMP] l10n_latam_check_ux: allow debit check wizard to handle multiple checks
    The `account.check.action.wizard` was previously only capable of handling a single check,
    using `active_id` in the context. This change updates the `action_confirm` method to iterate
    over all `active_ids`, allowing the wizard to be used with multiple selected checks.

--- l10n_latam_check_ux/wizards/account_check_action_wizard.py
+++ l10n_latam_check_ux/wizards/account_check_action_wizard.py
@@ -17,40 +17,47 @@ class AccountCheckActionWizard(models.TransientModel):
 
     def action_confirm(self):
         """Este método sirve para hacer el débito de cheques con cuenta outstanding desde los payments con método de pago de cheques."""
-        check = self.env["l10n_latam.check"].browse(self._context.get("active_id", False))
-        if self.date < check.payment_id.date:
-            raise UserError(
-                _("La fecha del débito del cheque %s no puede ser inferior a la fecha de emisión del mismo %s.")
-                % (self.date, check.payment_id.date)
+        checks = self.env["l10n_latam.check"].browse(self._context.get("active_ids", False))
+        if checks.filtered(lambda x: not x.check_add_debit_button):
+            raise UserError(_("At least one check is in a journal where the 'Add Debit Date' option is not enabled."))
+        for check in checks:
+            if self.date < check.payment_id.date:
+                raise UserError(
+                    _("La fecha del débito del cheque %s no puede ser inferior a la fecha de emisión del mismo %s.")
+                    % (self.date, check.payment_id.date)
+                )
+            # Línea del cheque a conciliar.
+            move_line_id = check.outstanding_line_id.ids
+            # Obtenemos la cuenta outstanding del método de pago pentientes "manual" del diario del pago o bien la "Cuenta de pagos pentientes" de la compañía.
+            outstanding_account = self._get_outstanding_account(check)
+            # Obtenemos fecha, importe, pasamos cuenta outstanding y el diario asignado es el mismo que se está editando.
+            label = f"Débito cheque nro {check.name}"
+            new_mv_line_dicts = {
+                "label": label,
+                "amount": abs(sum(check.outstanding_line_id.mapped("balance"))),
+                "account_id": outstanding_account.id,
+                "journal_id": check.original_journal_id.id,
+                "move_line_ids": move_line_id,
+                "date": self.date,
+            }
+            # Aquí hacemos el asiento del débito.
+            wizard = (
+                self.env["account.reconcile.wizard"]
+                .with_context(active_model="account.move.line", active_ids=move_line_id)
+                .create(new_mv_line_dicts)
             )
-        # Línea del cheque a conciliar.
-        move_line_id = check.outstanding_line_id.ids
-        # Obtenemos la cuenta outstanding del método de pago pentientes "manual" del diario del pago o bien la "Cuenta de pagos pentientes" de la compañía.
-        outstanding_account = self._get_outstanding_account(check)
-        # Obtenemos fecha, importe, pasamos cuenta outstanding y el diario asignado es el mismo que se está editando.
-        label = f"Débito cheque nro {check.name}"
-        new_mv_line_dicts = {
-            "label": label,
-            "amount": abs(sum(check.outstanding_line_id.mapped("balance"))),
-            "account_id": outstanding_account.id,
-            "journal_id": check.original_journal_id.id,
-            "move_line_ids": move_line_id,
-            "date": self.date,
-        }
-        # Aquí hacemos el asiento del débito.
-        wizard = (
-            self.env["account.reconcile.wizard"]
-            .with_context(active_model="account.move.line", active_ids=move_line_id)
-            .create(new_mv_line_dicts)
-        )
-        wizard.reconcile()
-        debit_move = self.env["account.move"].search([("line_ids.name", "=", label), ("date", "=", self.date)], limit=1)
-        if debit_move:
-            check.message_post(body=f'El cheque nro "{check.name}" ha sido debitado. ' + debit_move._get_html_link())
-        else:
-            check.message_post(
-                body=f'El cheque nro "{check.name}" ha sido debitado, pero no se encontró el asiento asociado.'
+            wizard.reconcile()
+            debit_move = self.env["account.move"].search(
+                [("line_ids.name", "=", label), ("date", "=", self.date)], limit=1
             )
+            if debit_move:
+                check.message_post(
+                    body=f'El cheque nro "{check.name}" ha sido debitado. ' + debit_move._get_html_link()
+                )
+            else:
+                check.message_post(
+                    body=f'El cheque nro "{check.name}" ha sido debitado, pero no se encontró el asiento asociado.'
+                )
 
     def _get_outstanding_account(self, check):
         """Obtenemos la cuenta para hacer el débito de cheques y hacemos las validaciones correspondientes. Siempre necesitamos que se encuentre establecido un método de pago manual en el diario para poder hacer el débito, no vamos a buscar la cuenta outstanding en configuración en caso de que no esté establecido el método de pago manual. Primero buscamos método de pago con code manual y nombre 'Manual' y si no lo encuentra buscamos el primer método de pago manual que se creó."""
