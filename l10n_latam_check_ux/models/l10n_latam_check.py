6ac89395226c77c1d1d4f07486c011907104639e
Rocío Vega <rov@adhoc.com.ar>
2025-06-05 11:17:47 -0300

    [IMP] l10n_latam_check_ux: Add date field
    In previous versions, checks were part of the `account.payment` model,
    which inherited from `account.move`.
    This inheritance included the `date` field,
    automatically setting the creation date of the check.
    Additionally, this date used to match with the date of the first operation associated with the check.
    
    With the current changes, the `date` field is explicitly added to ensure proper handling and consistency.

--- l10n_latam_check_ux/models/l10n_latam_check.py
+++ l10n_latam_check_ux/models/l10n_latam_check.py
@@ -1,7 +1,22 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
-class AccountPayment(models.Model):
+class l10nLatamAccountPaymentCheck(models.Model):
     _inherit = "l10n_latam.check"
 
     check_add_debit_button = fields.Boolean(related="original_journal_id.check_add_debit_button", readonly=True)
+    first_operation = fields.Many2one(
+        "account.payment",
+        compute="_compute_first_operation",
+        store=True,
+        readonly=True,
+    )
+    date = fields.Date(related="first_operation.date")
+
+    @api.depends("operation_ids.state")
+    def _compute_first_operation(self):
+        for rec in self:
+            all_operations = rec.payment_id + rec.operation_ids
+            valid_ops = all_operations.filtered(lambda x: x.state in ["in_process", "paid"])
+            sorted_ops = valid_ops.sorted(key=lambda payment: (payment.date, payment._origin.id))
+            rec.first_operation = sorted_ops[:1].id or False
