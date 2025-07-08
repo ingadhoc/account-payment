147ca1aae23d5d22bbc0d586dc04ca4d54f9a3f6
Rocío Vega <rov@adhoc.com.ar>
2025-07-08 15:31:04 -0300

    [IMP] l10n_latam_check_ux: Add operation date fields to order operations

--- l10n_latam_check_ux/models/account_payment.py
+++ l10n_latam_check_ux/models/account_payment.py
@@ -1,9 +1,13 @@
-from odoo import models
+from odoo import fields, models
 from odoo.exceptions import ValidationError
 
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
+
+    l10n_latam_move_check_ids_operation_date = fields.Datetime(
+        string="Operation Date",
+    )
 
     def action_post(self):
         # nosotros queremos bloquear tmb nros de cheques de terceros que sea unicos
@@ -14,6 +18,8 @@ class AccountPayment(models.Model):
         for rec in self:
             if rec.l10n_latam_check_warning_msg:
                 raise ValidationError("%s" % rec.l10n_latam_check_warning_msg)
+            if not rec.l10n_latam_move_check_ids_operation_date:
+                rec.l10n_latam_move_check_ids_operation_date = fields.Datetime.now()
         super().action_post()
 
     def _create_paired_internal_transfer_payment(self):
--- l10n_latam_check_ux/models/l10n_latam_check.py
+++ l10n_latam_check_ux/models/l10n_latam_check.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 
 
 class l10nLatamAccountPaymentCheck(models.Model):
@@ -20,3 +20,30 @@ class l10nLatamAccountPaymentCheck(models.Model):
             valid_ops = all_operations.filtered(lambda x: x.state in ["in_process", "paid"])
             sorted_ops = valid_ops.sorted(key=lambda payment: (payment.date, payment._origin.id))
             rec.first_operation = sorted_ops[:1].id or False
+
+    def button_open_check_operations(self):
+        action = super().button_open_check_operations()
+        self.ensure_one()
+        operations = self.operation_ids.sorted(lambda r: r.l10n_latam_move_check_ids_operation_date, reverse=True)
+        operations = (operations + self.payment_id).filtered(lambda x: x.state not in ["draft", "canceled"])
+        action = {
+            "name": _("Check Operations"),
+            "type": "ir.actions.act_window",
+            "res_model": "account.payment",
+            "views": [
+                (self.env.ref("l10n_latam_check.view_account_third_party_check_operations_tree").id, "list"),
+                (False, "form"),
+            ],
+            "context": {"create": False},
+            "domain": [("id", "in", operations.ids)],
+        }
+        return action
+
+    def _get_last_operation(self):
+        super()._get_last_operation()
+        self.ensure_one()
+        return (
+            (self.payment_id + self.operation_ids)
+            .filtered(lambda x: x.state not in ["draft", "canceled"])
+            .sorted(key=lambda payment: (payment.l10n_latam_move_check_ids_operation_date))[-1:]
+        )
