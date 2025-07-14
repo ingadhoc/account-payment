ad4c0844f5da4b78c0e3e0caa1526a9fc8581f03
Rocío Vega <rov@adhoc.com.ar>
2025-07-14 08:33:18 -0300

    [FIX] l10n_latam_check_ux: Fix post-migration script

--- l10n_latam_check_ux/migrations/18.0.1.6.0/post-migration.py
+++ l10n_latam_check_ux/migrations/18.0.1.6.0/post-migration.py
@@ -1,11 +1,8 @@
 import logging
 
-from openupgradelib import openupgrade
-
 _logger = logging.getLogger(__name__)
 
 
-@openupgrade.migrate()
 def migrate(cr, version):
     _logger.info("Forzamos la actualización de los l10n_latam_move_check_ids_operation_date")
     cr.execute("""
