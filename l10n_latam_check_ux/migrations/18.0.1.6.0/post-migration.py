d708dd5a007294bc6d0c3d9a7a23250d10b5f41c
Rocío Vega <rov@adhoc.com.ar>
2025-07-14 13:23:46 -0300

    [FIX] l10n_latam_check_ux: Fix migracion script for 18.0

--- l10n_latam_check_ux/migrations/18.0.1.6.0/post-migration.py
+++ l10n_latam_check_ux/migrations/18.0.1.6.0/post-migration.py
@@ -8,19 +8,6 @@ def migrate(cr, version):
     cr.execute("""
         UPDATE account_payment
         SET l10n_latam_move_check_ids_operation_date = create_date
-        WHERE payment_method_line_id IN (
-            SELECT id FROM account_payment_method_line
-            WHERE payment_method_id IN (
-                SELECT id FROM account_payment_method
-                WHERE code IN (
-                    'out_third_party_checks',
-                    'in_third_party_checks',
-                    'new_third_party_checks',
-                    'own_checks',
-                    'return_third_party_checks'
-                )
-            )
-        )
-        AND state NOT IN ('draft', 'canceled')
+        WHERE state NOT IN ('draft', 'canceled')
         AND l10n_latam_move_check_ids_operation_date IS NULL
     """)
