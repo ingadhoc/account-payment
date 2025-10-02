3fdcbd7371dd6bdc9223b234178b2a7e1af7c29b
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-02 18:04:45 +0000

    [MIG] l10n_latam_check_ux: Migration to 19.0

--- l10n_latam_check_ux/models/l10n_latam_check.py
+++ l10n_latam_check_ux/models/l10n_latam_check.py
@@ -5,37 +5,22 @@ class l10nLatamAccountPaymentCheck(models.Model):
     _inherit = "l10n_latam.check"
 
     check_add_debit_button = fields.Boolean(related="original_journal_id.check_add_debit_button", readonly=True)
-    first_operation = fields.Many2one(
-        "account.payment",
-        compute="_compute_first_operation",
-        store=True,
-        readonly=True,
-    )
-
-    date = fields.Date(related="first_operation.date")
+    date = fields.Date(related="payment_id.date")
     memo = fields.Char(related="payment_id.memo")
     company_id = fields.Many2one(
         compute="_compute_company_id", store=True, compute_sudo=True, comodel_name="res.company"
     )
-    operation_ids = fields.Many2many(check_company=False)
     payment_state = fields.Selection(
         related="payment_id.state",
         readonly=True,
     )
+    operation_ids = fields.Many2many(check_company=False)
 
     @api.depends("operation_ids.state", "payment_id.state")
     def _compute_company_id(self):
         for rec in self:
             last_operation = rec._get_last_operation() or rec.payment_id
             rec.company_id = last_operation.company_id
-
-    @api.depends("operation_ids.state", "payment_id.state")
-    def _compute_first_operation(self):
-        for rec in self:
-            all_operations = rec.payment_id + rec.operation_ids
-            valid_ops = all_operations.filtered(lambda x: x.state in ["in_process", "paid"])
-            sorted_ops = valid_ops.sorted(key=lambda payment: (payment.date, payment._origin.id))
-            rec.first_operation = sorted_ops[:1].id or False
 
     def button_open_check_operations(self):
         action = super(l10nLatamAccountPaymentCheck, self.sudo()).button_open_check_operations()
