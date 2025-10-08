b90c5a21da4388f8c5ddd481438ca1b400a47d3b
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2025-10-08 19:49:24 +0000

    [MIG] account_payment_ux: Migration to 19.0
    closes ingadhoc/account-payment#788
    
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- account_payment_ux/models/account_payment.py
+++ account_payment_ux/models/account_payment.py
@@ -6,10 +6,10 @@ class AccountPayment(models.Model):
 
     def action_post(self):
         """Odoo a partir de 16, cuando se valida un pago con token, si la transaccion no queda en done cancela el pago
-        por ahora nosotros revertimos este cambio para el caso de tu cuota"""
+        por ahora nosotros revertimos este cambio"""
         return super(AccountPayment, self.with_context(from_action_post=True)).action_post()
 
     def action_cancel(self):
-        if self._context.get("from_action_post"):
+        if self.env.context.get("from_action_post"):
             self = self - self.filtered(lambda x: x.payment_transaction_id.state in ["draft", "pending", "authorized"])
         return super(AccountPayment, self).action_cancel()
