38135e3b4d1b2763a9518763df1634c5eae09839
Rocío Vega <rov@adhoc.com.ar>
2025-10-27 11:58:38 -0300

    [IMP] l10n_latam_check_ux: Add checks to credit limit method calculation
    closes ingadhoc/account-payment#807
    
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>

--- l10n_latam_check_ux/__manifest__.py
+++ l10n_latam_check_ux/__manifest__.py
@@ -39,6 +39,7 @@
         "views/l10n_latam_check_view.xml",
         "views/account_journal_view.xml",
         "views/report_payment_receipt_templates.xml",
+        "views/res_partner_view.xml",
         "wizards/l10n_latam_payment_mass_transfer.xml",
         "reports/report_account_transfer.xml",
         "reports/report_checks_to_date.xml",
