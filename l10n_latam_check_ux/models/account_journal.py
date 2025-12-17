c3c2b35373f62800be5f5b3260e3022744bf1a41
Virginia <vib@adhoc.inc>
2025-12-17 14:12:34 -0300

    Update project.toml from template

--- l10n_latam_check_ux/models/account_journal.py
+++ l10n_latam_check_ux/models/account_journal.py
@@ -8,5 +8,11 @@ class AccountJournal(models.Model):
         string="Agregar botón de débito",
         store=True,
         readonly=False,
-        help="Si marca esta opción podrá debitar los cheques con un botón desde los mismos. Para realizar el asiento de débito se buscará un método de pago saliente del tipo Manual con nombre Manual, si no se encuentra uno se utilizará el primero que sea del tipo Manual (sin importar el nombre). Se utilizará luego la cuenta configurada en dicho método de ese método de pago.",
+        help=(
+            "Si marca esta opción podrá debitar los cheques con un botón desde los mismos. "
+            "Para realizar el asiento de débito se buscará un método de pago saliente del tipo "
+            "Manual con nombre Manual, si no se encuentra uno se utilizará el primero que sea "
+            "del tipo Manual (sin importar el nombre). Se utilizará luego la cuenta configurada "
+            "en dicho método de ese método de pago."
+        ),
     )
