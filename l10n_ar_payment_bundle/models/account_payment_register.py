cf5ca81a8deb2c17f3c7774f1025f06548dc49b4
roboadhoc <roboadhoc@adhoc.com.ar>
2026-03-05 15:36:07 +0000

    [FIX] l10n_ar_payment_bundle: Added account_payment_register when Payment pro is disabled
    closes ingadhoc/account-payment#953
    
    X-original-commit: b8cb0293b5bbe03af257dab2299c61f4884003c6
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

---
+++ l10n_ar_payment_bundle/models/account_payment_register.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models
+
+
+class AccountPaymentRegister(models.TransientModel):
+    """
+    Filtramos el diario de bundle (paquete de pagos) de los diarios disponibles
+    en el asistente de pagos cuando la compañía no usa Payment Pro, para evitar
+    que se seleccione manualmente un diario que debe usarse solo desde el formulario.
+    """
+
+    _inherit = "account.payment.register"
+
+    use_payment_pro = fields.Boolean(compute="_compute_use_payment_pro")
+
+    def _compute_use_payment_pro(self):
+        payment_with_pro = self.filtered(lambda x: x.company_id.use_payment_pro and x.outstanding_account_id)
+        payment_with_pro.use_payment_pro = True
+        (self - payment_with_pro).use_payment_pro = False
+
+    @api.depends("available_journal_ids", "company_id")
+    def _compute_available_journal_ids(self):
+        super()._compute_available_journal_ids()
+        for rec in self.filtered(lambda x: x.company_id and not x.use_payment_pro):
+            bundle_journal_id = rec.company_id._get_bundle_journal(rec.payment_type)
+            rec.available_journal_ids = rec.available_journal_ids.filtered(lambda x: x._origin.id != bundle_journal_id)
