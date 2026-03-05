a6f3f1b8536557b8416961f789769464936e0be9
roboadhoc <roboadhoc@adhoc.com.ar>
2026-03-05 15:36:07 +0000

    [BOT] Bump version: account_cashbox_bundle 18.0.1.1.0, account_cashbox 18.0.1.3.0
    Merged: ingadhoc/account-payment#930

--- account_cashbox_bundle/__manifest__.py
+++ account_cashbox_bundle/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Cashbox Bundle",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Accounting",
     "summary": "Technical bridge module for account_cashbox and l10n_ar_payment_bundle",
     "author": "ADHOC SA",
