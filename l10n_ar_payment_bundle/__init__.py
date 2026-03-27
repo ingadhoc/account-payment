ceec1b5f2722d5c5c09c9d6821d6d779e093b54f
Martin Quinteros <maq@adhoc.com.ar>
2026-03-27 14:17:26 -0300

    [IMP] l10n_ar_payment_bundle: broaden AR company detection to country code
    Use partner country code (AR) instead of specific chart of accounts
    template codes (ar_ri, ar_ex, ar_base) to detect Argentine companies,
    both in the post_init_hook and in the payment bundle journal template.
    
    This ensures the payment bundle journal is created for all Argentine
    companies regardless of which chart of accounts they use.

--- l10n_ar_payment_bundle/__init__.py
+++ l10n_ar_payment_bundle/__init__.py
@@ -3,8 +3,7 @@ from . import models
 
 def post_init_hook(env):
     """Existing companies that have the Argentinean Chart of Accounts set"""
-    template_codes = ["ar_ri", "ar_ex", "ar_base"]
-    ar_companies = env["res.company"].search([("chart_template", "in", template_codes), ("parent_id", "=", False)])
+    ar_companies = env["res.company"].search([("partner_id.country_id.code", "=", "AR"), ("parent_id", "=", False)])
     for company in ar_companies:
         template_code = company.chart_template
         ChartTemplate = env["account.chart.template"].with_company(company)
