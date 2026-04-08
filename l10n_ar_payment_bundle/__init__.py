d8c66dd325e3ed51fa0d2f1fb696e461ec8cc333
Rocío Vega <rov@adhoc.com.ar>
2026-04-08 13:50:52 +0000

    [IMP] l10n_ar_payment_bundle: auto-create payment bundle journal on init, create, and use_payment_pro enablement
    automatically create bundle journal for companies with Payment Pro enabled
    generate bundle journal on module install and company setting toggle
    create bundle journal when Payment Pro is enabled
    
    X-original-commit: 417b5da5c6853d3dd6bef6426ef86a6cb9caab86

--- l10n_ar_payment_bundle/__init__.py
+++ l10n_ar_payment_bundle/__init__.py
@@ -3,10 +3,5 @@ from . import wizard
 
 
 def post_init_hook(env):
-    """Existing companies that have the Argentinean Chart of Accounts set"""
-    ar_companies = env["res.company"].search([("partner_id.country_id.code", "=", "AR"), ("parent_id", "=", False)])
-    for company in ar_companies:
-        template_code = company.chart_template
-        ChartTemplate = env["account.chart.template"].with_company(company)
-        if journals_to_create := ChartTemplate._get_payment_bundle_account_journal(template_code):
-            ChartTemplate._load_data({"account.journal": journals_to_create})
+    companies = env["res.company"].search([("active", "=", True), ("use_payment_pro", "=", True)])
+    companies._create_payment_bundle_journal_if_needed()
