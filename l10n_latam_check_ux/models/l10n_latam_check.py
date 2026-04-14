9cd17867e7c46e8d92b681de4d926b93bdc77fb5
roboadhoc <roboadhoc@adhoc.com.ar>
2026-04-14 18:06:11 +0000

    [BOT] Bump version: account_payment_pro 19.0.2.5.0, l10n_ar_payment_bundle 19.0.1.8.0
    closes ingadhoc/account-payment#978
    
    Merged: ingadhoc/account-payment#1011
    Signed-off-by: Filoquin adhoc <maq@adhoc.com.ar>

--- l10n_latam_check_ux/models/l10n_latam_check.py
+++ l10n_latam_check_ux/models/l10n_latam_check.py
@@ -7,6 +7,9 @@ class l10nLatamAccountPaymentCheck(models.Model):
     _unique = models.UniqueIndex("(name, payment_method_line_id) WHERE issue_state IS NOT NULL")
 
     check_add_debit_button = fields.Boolean(related="original_journal_id.check_add_debit_button", readonly=True)
+    can_reject = fields.Boolean(
+        compute="_compute_can_reject",
+    )
     date = fields.Date(related="payment_id.date")
     memo = fields.Char(related="payment_id.memo")
     company_id = fields.Many2one(
@@ -74,3 +77,42 @@ class l10nLatamAccountPaymentCheck(models.Model):
             account = rec.outstanding_line_id.account_id
             if account and not account.reconcile:
                 rec.issue_state = "debited"
+
+    @api.depends(
+        "payment_method_line_id",
+        "payment_id.state",
+        "operation_ids.state",
+        "operation_ids.payment_method_line_id",
+        "current_journal_id",
+        "original_journal_id",
+    )
+    def _compute_can_reject(self):
+        for rec in self:
+            if rec.payment_method_code != "new_third_party_checks" or rec.payment_id.state in ("draft", "cancel"):
+                rec.can_reject = False
+                continue
+
+            # Already returned to customer (rejection already processed).
+            # We include both return_third_party_checks and out_third_party_checks because
+            # some journals might not have the specific "return" method configured.
+            if any(
+                op.state == "posted"
+                and op.payment_type == "outbound"
+                and op.partner_type == "customer"
+                and op.payment_method_code in ("return_third_party_checks", "out_third_party_checks")
+                for op in rec.operation_ids
+            ):
+                rec.can_reject = False
+                continue
+
+            last_op = rec._get_last_operation()
+            if not last_op:
+                rec.can_reject = False
+                continue
+
+            # Case A: endorsed to a vendor (last outbound op with out_third_party_checks)
+            endorsed = last_op.payment_method_code == "out_third_party_checks"
+            # Case B: deposited/transferred to another journal (check is in a different journal than original)
+            deposited = rec.current_journal_id != rec.original_journal_id
+
+            rec.can_reject = endorsed or deposited
---
+++ l10n_latam_check_ux/wizards/account_check_reject_wizard.py
@@ -0,0 +1,247 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from datetime import timedelta
+
+from odoo import Command, _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class AccountCheckRejectWizard(models.TransientModel):
+    _name = "account.check.reject.wizard"
+    _description = "Reject Third Party Check Wizard"
+
+    date = fields.Date(
+        default=fields.Date.context_today,
+        required=True,
+    )
+    rejected_journal_id = fields.Many2one(
+        "account.journal",
+        string="Rejected Checks Journal",
+        required=True,
+        domain=[("type", "in", ("bank", "cash"))],
+        help="Journal used for rejected third-party checks (e.g. 'Cheques de Terceros Rechazados').",
+    )
+    case_description = fields.Char(
+        compute="_compute_case_description",
+        string="Action Summary",
+    )
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        checks = self.env["l10n_latam.check"].browse(self._context.get("active_ids", []))
+        for check in checks:
+            if not check.can_reject:
+                raise UserError(
+                    _("Check '%s' cannot be rejected. Only third-party checks in valid states can be rejected.")
+                    % check.name
+                )
+        return res
+
+    @api.depends("rejected_journal_id")
+    def _compute_case_description(self):
+        for rec in self:
+            checks = rec._get_checks()
+            if not checks:
+                rec.case_description = False
+                continue
+            endorsed = checks.filtered(lambda c: rec._get_case_type(c) == "endorsed")
+            deposited = checks.filtered(lambda c: rec._get_case_type(c) == "deposited")
+            parts = []
+            if endorsed:
+                parts.append(
+                    _("%d endorsed check(s): supplier receipt + customer debit will be created.") % len(endorsed)
+                )
+            if deposited:
+                parts.append(
+                    _("%d deposited check(s): bank outgoing transfer + customer debit will be created.")
+                    % len(deposited)
+                )
+            rec.case_description = "  ".join(parts) if parts else False
+
+    def _get_checks(self):
+        return self.env["l10n_latam.check"].browse(self._context.get("active_ids", []))
+
+    def _get_case_type(self, check):
+        """Return 'endorsed' if the check was given to a supplier, 'deposited' if it is in a bank journal."""
+        if check.current_journal_id and check.current_journal_id != check.original_journal_id:
+            return "deposited"
+        return "endorsed"
+
+    def action_confirm(self):
+        checks = self._get_checks()
+        if not checks:
+            raise UserError(_("No checks selected."))
+
+        for check in checks:
+            last_operation = check._get_last_operation()
+            if (
+                check.payment_method_code != "new_third_party_checks"
+                or not check.can_reject
+                or last_operation.journal_id == self.rejected_journal_id
+            ):
+                raise UserError(_("Check '%s' is not a third-party check and cannot be rejected.") % check.name)
+            self._reject_check(check)
+
+        # Redirect to check operations view
+        return self._open_check_operations(checks)
+
+    def _reject_check(self, check):
+        case_type = self._get_case_type(check)
+        created_payments = self.env["account.payment"]
+
+        if case_type == "endorsed":
+            recovery_payment = self._create_endorsed_recovery_payment(check)
+            recovery_payment.action_post()
+            created_payments |= recovery_payment
+        else:
+            bank_payment = self._create_bank_transfer_payment(check)
+            bank_payment.action_post()
+            paired = bank_payment.paired_internal_transfer_payment_id
+            created_payments |= bank_payment
+            if paired:
+                created_payments |= paired
+
+        customer_payment = self._create_customer_return_payment(check)
+        customer_payment.action_post()
+
+        # Ensure customer debit is the last operation by giving it a later operation date
+        operation_dates = created_payments.filtered(lambda p: p.l10n_latam_move_check_ids_operation_date).mapped(
+            "l10n_latam_move_check_ids_operation_date"
+        )
+        if operation_dates:
+            customer_payment.l10n_latam_move_check_ids_operation_date = max(operation_dates) + timedelta(seconds=3)
+
+    def _create_endorsed_recovery_payment(self, check):
+        """Case A: Create inbound payment in rejected_journal to recover the check from the endorsee (vendor)."""
+        inbound_method = self.rejected_journal_id._get_available_payment_method_lines("inbound").filtered(
+            lambda x: x.code == "in_third_party_checks"
+        )[:1]
+        if not inbound_method:
+            raise UserError(
+                _(
+                    "The journal '%s' needs an inbound payment method of type 'Third Party Check (in)' "
+                    "to receive rejected checks from the supplier."
+                )
+                % self.rejected_journal_id.display_name
+            )
+
+        last_operation = check._get_last_operation()
+        vendor = last_operation.partner_id if last_operation else check.payment_id.partner_id
+
+        memo = _("Check rejection - recovery from supplier: %s") % check.name
+        return self.env["account.payment"].create(
+            {
+                "date": self.date,
+                "amount": check.amount,
+                "currency_id": check.currency_id.id,
+                "partner_id": vendor.id,
+                "payment_type": "inbound",
+                "partner_type": "supplier",
+                "journal_id": self.rejected_journal_id.id,
+                "payment_method_line_id": inbound_method.id,
+                "memo": memo,
+                "l10n_latam_move_check_ids": [Command.link(check.id)],
+            }
+        )
+
+    def _create_bank_transfer_payment(self, check):
+        """Case B: Create an outgoing transfer (internal transfer) from the bank to the rejected_journal."""
+        bank_journal = check.current_journal_id
+        out_method = bank_journal._get_available_payment_method_lines("outbound").filtered(
+            lambda x: x.code in ("out_third_party_checks", "return_third_party_checks")
+        )[:1]
+        if not out_method:
+            raise UserError(
+                _(
+                    "The journal '%s' needs an outbound payment method compatible with third-party checks "
+                    "(e.g. 'out_third_party_checks') to transfer the rejected check."
+                )
+                % bank_journal.display_name
+            )
+
+        in_method = self.rejected_journal_id._get_available_payment_method_lines("inbound").filtered(
+            lambda x: x.code == "in_third_party_checks"
+        )[:1]
+        if not in_method:
+            raise UserError(
+                _(
+                    "The journal '%s' needs an inbound payment method of type 'Third Party Check (in)' "
+                    "to receive the transferred rejected check."
+                )
+                % self.rejected_journal_id.display_name
+            )
+
+        memo = _("Check rejection - bank transfer: %s") % check.name
+        return self.env["account.payment"].create(
+            {
+                "date": self.date,
+                "amount": check.amount,
+                "currency_id": check.currency_id.id,
+                "partner_id": self.env.company.partner_id.id,
+                "payment_type": "outbound",
+                "journal_id": bank_journal.id,
+                "is_internal_transfer": True,
+                "destination_journal_id": self.rejected_journal_id.id,
+                "payment_method_line_id": out_method.id,
+                "memo": memo,
+                "l10n_latam_move_check_ids": [Command.link(check.id)],
+            }
+        )
+
+    def _create_customer_return_payment(self, check):
+        """Create outbound payment from rejected_journal to return the check to the customer (generates AR debt)."""
+        available_outbound_methods = self.rejected_journal_id._get_available_payment_method_lines("outbound")
+        return_method = available_outbound_methods.filtered(lambda x: x.code == "return_third_party_checks")[:1]
+        if not return_method:
+            return_method = available_outbound_methods.filtered(lambda x: x.code == "out_third_party_checks")[:1]
+        if not return_method:
+            raise UserError(
+                _(
+                    "The journal '%s' needs an outbound payment method of type 'Return Third Party Check' "
+                    "or 'Third Party Check (out)' to return the rejected check to the customer and generate the debt."
+                )
+                % self.rejected_journal_id.display_name
+            )
+
+        customer = check.payment_id.partner_id
+        memo = _("Check rejection - customer debit: %s") % check.name
+        return self.env["account.payment"].create(
+            {
+                "date": self.date,
+                "amount": check.amount,
+                "currency_id": check.currency_id.id,
+                "partner_id": customer.id,
+                "to_pay_move_line_ids": False,
+                "payment_type": "outbound",
+                "partner_type": "customer",
+                "journal_id": self.rejected_journal_id.id,
+                "payment_method_line_id": return_method.id,
+                "memo": memo,
+                "l10n_latam_move_check_ids": [Command.link(check.id)],
+            }
+        )
+
+    def _open_check_operations(self, checks):
+        """Open the check operations view for the rejected checks."""
+        # Collect all operations from all processed checks
+        all_operations = self.env["account.payment"]
+        for check in checks:
+            operations = (check.operation_ids + check.payment_id).filtered(
+                lambda x: x.state not in ["draft", "canceled"]
+            )
+            all_operations |= operations
+
+        return {
+            "name": _("Check Operations"),
+            "type": "ir.actions.act_window",
+            "res_model": "account.payment",
+            "views": [
+                (self.env.ref("l10n_latam_check.view_account_third_party_check_operations_tree").id, "list"),
+                (False, "form"),
+            ],
+            "context": {"create": False},
+            "domain": [("id", "in", all_operations.ids)],
+        }
