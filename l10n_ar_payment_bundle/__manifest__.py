604ac83efd710c1bfc074547e047d56fd7c223ce
roboadhoc <roboadhoc@adhoc.com.ar>
2026-04-14 18:06:11 +0000

    [BOT] Bump version: account_payment_pro 19.0.2.5.0, l10n_ar_payment_bundle 19.0.1.8.0
    Merged: ingadhoc/account-payment#1011

--- l10n_ar_payment_bundle/__manifest__.py
+++ l10n_ar_payment_bundle/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Argentinean Payment bundle",
-    "version": "19.0.1.7.0",
+    "version": "19.0.1.8.0",
     "category": "Payment",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
