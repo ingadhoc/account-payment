b7969aaa5bab8da2292111b20d31ebefe0d18ed9
Camila Vives <cav@adhoc.com.ar>
2026-05-12 16:15:46 -0300

    [FIX] account_payment_pro: keep posted payment accounting rate

--- account_payment_pro/tests/test_account_paymet_pro_unit_test.py
+++ account_payment_pro/tests/test_account_paymet_pro_unit_test.py
@@ -169,6 +169,36 @@ class TestAccountPaymentProUnitTest(common.TransactionCase):
             msg="Liquidity line balance should still use forced amount after synchronization",
         )
 
+    def test_posted_payment_without_payment_pro_keeps_accounting_rate_after_rate_change(self):
+        self.company.use_payment_pro = False
+
+        payment = self.env["account.payment"].create(
+            {
+                "payment_type": "inbound",
+                "partner_type": "customer",
+                "partner_id": self.partner_ri.id,
+                "journal_id": self.company_bank_journal.id,
+                "amount": 100.0,
+                "currency_id": self.eur_currency.id,
+                "date": self.today,
+            }
+        )
+        payment.action_post()
+
+        liquidity_lines = payment.move_id.line_ids.filtered(
+            lambda line: line.account_id == payment.outstanding_account_id
+        )
+        accounting_amount = abs(sum(liquidity_lines.mapped("balance")))
+        self.assertAlmostEqual(accounting_amount, 100000.0, places=2)
+        self.assertAlmostEqual(payment.amount_company_currency, accounting_amount, places=2)
+        self.assertAlmostEqual(payment.exchange_rate, 1000.0, places=2)
+
+        self.rates[1].inverse_company_rate = 2000
+        payment.invalidate_recordset(["amount_company_currency", "exchange_rate"])
+
+        self.assertAlmostEqual(payment.amount_company_currency, accounting_amount, places=2)
+        self.assertAlmostEqual(payment.exchange_rate, 1000.0, places=2)
+
     def test_write_off_line_amounts_company_vs_payment_currency(self):
         """Minimal test: company currency vs payment currency, force company amount and check write-off line balance"""
         # Use existing company and ensure we have a different currency for the payment
