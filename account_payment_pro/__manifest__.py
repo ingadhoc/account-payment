a3f81348550003aa947eaa810f8c3c38a1d760bc
roboadhoc <roboadhoc@adhoc.com.ar>
2026-09-02 13:58:48 +0000

    [BOT] Bump version: l10n_ar_payment_bundle 19.0.1.13.0, account_payment_pro 19.0.2.12.0, l10n_latam_check_ux 19.0.1.9.0
    Merged: ingadhoc/account-payment#1194, ingadhoc/account-payment#1171

--- account_payment_pro/__manifest__.py
+++ account_payment_pro/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Payment Super Power",
-    "version": "19.0.2.11.1",
+    "version": "19.0.2.12.0",
     "category": "Payment",
     "website": "www.adhoc.com.ar",
     "author": "ADHOC SA",
