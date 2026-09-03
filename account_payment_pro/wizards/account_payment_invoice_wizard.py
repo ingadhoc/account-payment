3eb38810b7ef3ccae18bdc088dc3cedad784ad08
Camila Vives <cav@adhoc.com.ar>
2026-09-03 13:30:53 +0000

    chore: dummy commit to trigger CI/runbot

--- account_payment_pro/wizards/account_payment_invoice_wizard.py
+++ account_payment_pro/wizards/account_payment_invoice_wizard.py
@@ -3,6 +3,7 @@
 ##############################################################################
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
+# dummy change to trigger CI/runbot
 
 
 class AccountPaymentInvoiceWizard(models.TransientModel):
